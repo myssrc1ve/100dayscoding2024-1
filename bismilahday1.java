2eeffab9dec2a127dae85b430871bd66dcb79c05
afvgn <isfanurmaisyarah@gmail.com>
2024-09-02 22:16:24 +0800

    Create bismilahday1.java

---
+++ bismilahday1.java
@@ -0,0 +1,26 @@
+public class Main {
+	public static void main(String[] args) {
+
+		String nama = "Prabu Aliafgan";
+		String ttl = "Totolisi, 9 September";
+		String jenisKelamin = "Laki-laki";
+		int umur = 20;
+		String alamat = "Totolisi";
+		String sma = "SMA PPM Al Ikhlash";
+		String kampus = "Universitas Sulawesi Barat";
+		String fakultas = "Teknik";
+		String prodi = "Teknik Informatika";
+		String nim = "D0222328";
+
+		System.out.println("Nama = " + nama);
+		System.out.println("TTL = " + ttl);
+		System.out.println("Jenis Kelamin = " + jenisKelamin);
+		System.out.println("Umur = " + umur);
+		System.out.println("Alamat = " + alamat);
+		System.out.println("SMA = " + sma);
+		System.out.println("Kampus = " + kampus);
+		System.out.println("Fakultas = " + fakultas);
+		System.out.println("Prodi = " + prodi);
+		System.out.println("NIM = " + nim);
+	}
+}
