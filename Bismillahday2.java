13c0b1329f3fa1adb052e9ae5b1e8b66e9eaa406
afvgn <isfanurmaisyarah@gmail.com>
2024-09-03 19:47:03 +0800

    Create Bismillahday2.java

---
+++ Bismillahday2.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        String namaProduk = "Laptop";
+        double harga = 7500000.50;
+        int stok = 10;
+        String kategori = "Elektronik";
+
+        // Jadi fungsinnya printf ini untuk mencetak informasi dengan format tertentu
+        System.out.printf("Nama Produk: %s\n", namaProduk);
+        System.out.printf("Harga: Rp%.2f\n", harga);
+        System.out.printf("Stok: %d unit\n", stok);
+        System.out.printf("Kategori: %s\n", kategori);
+    }
+}
