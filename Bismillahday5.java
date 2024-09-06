2cc3b0adb815a5a887302d7b2bc4cecb56dd8fda
afvgn <isfanurmaisyarah@gmail.com>
2024-09-06 23:59:25 +0800

    Create Bismillahday5.java

---
+++ Bismillahday5.java
@@ -0,0 +1,19 @@
+public class Komentar {
+
+    public static void main(String[] args) {
+
+   
+        String nama = "Prabu Aliafgan";
+        String Nim = "D0222328";
+        char kelas = 'A';
+        String jurusan = "Informatika";
+        int angkatan = 2022;
+
+        System.out.println("Nama = " + nama); // Digunakan untuk mencetak data dan menambahkan baris baru
+        System.out.printf("NIM = %s \n", Nim); // Digunakan untuk mencetak teks/data dengan format tertentu 
+        System.out.print("Jurusan = " + jurusan + "\n"); // Digunakan untuk mencetak teks dalam bentuk baris
+        System.out.println("Kelas = " + kelas); // Digunakan untuk mencetak data
+        System.out.println("Angkatan = " + angkatan); // Digunakan untuk mencetak data angka
+    }
+
+}
