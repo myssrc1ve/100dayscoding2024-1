5250ffd844bc1d64bab1508a04e880a8e5254171
afvgn <isfanurmaisyarah@gmail.com>
2024-09-07 21:30:01 +0800

    Create bismillahday6.java

---
+++ bismillahday6.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Deklarasi variabel dengan nilai
+        String nama = "Majene";         // Tipe data String
+        int jumlahPenduduk = 75000;     // Tipe data integer
+        double luasWilayah = 947.84;    // Tipe data double, dalam kilometer persegi
+
+        // Menampilkan nilai variabel
+        System.out.println("Nama: " + nama);
+        System.out.println("Jumlah Penduduk: " + jumlahPenduduk);
+        System.out.println("Luas Wilayah: " + luasWilayah + " km²");
+    }
+}
