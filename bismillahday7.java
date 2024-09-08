88040c280e03a8426c27784c4f9a101ec4aada9c
afvgn <isfanurmaisyarah@gmail.com>
2024-09-08 23:17:21 +0800

    Create bismillahday7.java

---
+++ bismillahday7.java
@@ -0,0 +1,17 @@
+public class InisialisasiSederhana {
+    public static void main(String[] args) {
+        // Inisialisasi variabel dengan tipe data yang berbeda
+        int angka = 10;               
+        double desimal = 20.5;         
+        char karakter = 'A';           
+        String teks = "Hello, Java!";  
+        boolean benar = true;   
+
+        // Menampilkan nilai dari setiap variabel
+        System.out.println("Nilai dari angka: " + angka);
+        System.out.println("Nilai dari desimal: " + desimal);
+        System.out.println("Nilai dari karakter: " + karakter);
+        System.out.println("Nilai dari teks: " + teks);
+        System.out.println("Nilai dari boolean: " + benar);
+    }
+}
