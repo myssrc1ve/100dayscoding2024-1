eefc6e9a1067a2d61030fed33dd714ea46a79aa0
afvgn <isfanurmaisyarah@gmail.com>
2024-09-11 23:28:08 +0800

    Create bismillahday10.java

---
+++ bismillahday10.java
@@ -0,0 +1,11 @@
+public class contohtipedataByte {
+    public static void main(String[] args) {
+        // Deklarasi variabel tipe data byte
+        byte age = 25;
+        byte temperature = -5;
+
+        // Menampilkan nilai variabel
+        System.out.println("Usia: " + age);
+        System.out.println("Suhu: " + temperature + " derajat");
+    }
+}
