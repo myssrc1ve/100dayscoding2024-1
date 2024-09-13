ef1ec34df207c7273b3b77bb92284326a1fa960c
afvgn <isfanurmaisyarah@gmail.com>
2024-09-13 19:38:18 +0800

    Create bismillahday12_integer.java

---
+++ bismillahday12_integer.java
@@ -0,0 +1,21 @@
+public class tipedatainteger {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Nilai a: " + a);
+        System.out.println("Nilai b: " + b);
+
+        int jumlah = a + b;
+        int selisih = a - b;
+        int produk = a * b;
+        int pembagian = a / b;
+        int sisaBagi = a % b;
+
+        System.out.println("Jumlah a dan b: " + jumlah);
+        System.out.println("Selisih a dan b: " + selisih);
+        System.out.println("Produk a dan b: " + produk);
+        System.out.println("Pembagian a dengan b: " + pembagian);
+        System.out.println("Sisa bagi a dengan b: " + sisaBagi);
+    }
+}
