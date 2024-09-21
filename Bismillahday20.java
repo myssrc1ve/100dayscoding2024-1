d2f1216d2b52f978a2f32657ddb3559cd43c86b3
afvgn <isfanurmaisyarah@gmail.com>
2024-09-21 21:52:10 +0800

    Update

---
+++ Bismillahday20.java
@@ -0,0 +1,14 @@
+public class ContohPenjumlahanByte {
+    public static void main(String[] args) {
+        // Menggunakan tipe data referensi Byte
+        Byte byte1 = 10;
+        Byte byte2 = 20;
+        Byte byte3 = 30;
+
+        // Menghitung total
+        int total = byte1 + byte2 + byte3;
+
+        // Menampilkan hasil
+        System.out.println("Total nilai: " + total);
+    }
+}
