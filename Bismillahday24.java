ac4e6a9b50590923a18a2bbf8d147df519e5842b
afvgn <isfanurmaisyarah@gmail.com>
2024-09-25 21:31:39 +0800

    Update

---
+++ Bismillahday24.java
@@ -0,0 +1,10 @@
+public class contohFloat {
+    public static void main(String[] args) {
+        Float number = 10.5f;
+        System.out.println("Nilai Float: " + number);
+        
+        Float anotherNumber = 5.5f;
+        Float sum = number + anotherNumber;
+        System.out.println("Jumlah: " + sum);
+    }
+}
